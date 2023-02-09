4f2acf1e586963d75a3c9caa8b022b009632695e
priced24 <priced24@up.edu>
2023-02-08 16:04:42 -0800

    sanity check

---
+++ hw3/public/javascripts/stuff.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // when order button is pressed
+    $("#send-order").click(function () {
+        notes = $("#notes").val();
+        count = $("#count").val();
+
+        // if notes contain substring "vegan"
+        if ($("#notes").val().includes('vegan')) {
+            alert("All cheesecakes contain dairy."); // alert will display at top of browser window
+        }
+        else { // hide the order menu and total orders, display user's order and notes
+            flavor = $("input[name='topping']:checked").val();
+            $("#order-toppings").text('Topping: ' + flavor);
+            $("#order-count").text('Quantity: ' + count);
+            $("#order-notes").text('Notes: ' + notes);
+            $("#to-clear").hide();
+            $("#to-display").show();
+        }
+    });
+
+    // this will change the text of the hover dropdown menu button
+    $("#dropdown").click(function (event) {
+        $("#dropbtn").html("<b>" + $(event.target).text() + "</b>");
+    });
+});
